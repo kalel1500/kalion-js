b77669a19745a26c17a2a6f3dfa612be0a75aa60
Adrian Canals <a.canals14@gmail.com>
2025-02-28 12:43:21 +0100

    (fix) Mover la variable "useBootstrap" dentro del método "Component.get()" ya que al estar fuera no se sobreescribía bien desde la aplicación

--- src/app/core/infrastructure/utilities/components/Component.ts
+++ src/app/core/infrastructure/utilities/components/Component.ts
@@ -4,15 +4,14 @@ import { Icon } from './html/Icon';
 import { ComponentName, ComponentType, ConfigBtn, ConfigIcon, IconType } from '../../../_types';
 
 export class Component {
-    static useBootstrap = __const('VITE_TS_USE_BOOSTRAP_CLASSES');
-
     static get<T extends ComponentName>(component: T, config?: T extends `btn` ? ConfigBtn : ConfigIcon): string {
         const [type, name] = component.split('.') as [ComponentType, string];
+        const useBootstrap = __const('VITE_TS_USE_BOOSTRAP_CLASSES');
 
         if (type === 'btn') {
-            return this.useBootstrap ? Btn.bootstrap(config as ConfigBtn) : Btn.tailwind(config as ConfigBtn);
+            return useBootstrap ? Btn.bootstrap(config as ConfigBtn) : Btn.tailwind(config as ConfigBtn);
         } else if (type === 'icon') {
-            return this.useBootstrap ? Icon.bootstrap(name as IconType) : Icon.tailwind(name as IconType, config as ConfigIcon);
+            return useBootstrap ? Icon.bootstrap(name as IconType) : Icon.tailwind(name as IconType, config as ConfigIcon);
         }
         return '';
     }
