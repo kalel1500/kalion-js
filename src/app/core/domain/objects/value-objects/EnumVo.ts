c91cce19df60ffa0d034a02d2732b143232d02d9
Adrian Canals <a.canals14@gmail.com>
2026-03-03 21:44:04 +0100

    (breaking) rediseñar "EnumVo" como clase genérica para mejorar el tipado estricto y simplificar la lógica interna eliminando la gestión manual de nulos y el método setPermittedValues en favor de una propiedad abstracta T[].

--- src/app/core/domain/objects/value-objects/EnumVo.ts
+++ src/app/core/domain/objects/value-objects/EnumVo.ts
@@ -1,49 +1,25 @@
-import { g, InvalidValueException, StrOrNullish } from '@/app';
+import { InvalidValueException, StrIntOrNullish } from '@/app';
 
-export type EnumVoParams = {
-    allowNull?: boolean
-}
+export abstract class EnumVo<T extends StrIntOrNullish> {
+    public value: T;
+    protected abstract _permittedValues: T[];
 
-export abstract class EnumVo {
-    protected _value: StrOrNullish;
-    protected _allowNull: boolean;
-    protected _permittedValues: string[] = [];
-
-    constructor(value: StrOrNullish, params?: EnumVoParams) {
-        this.setPermittedValues();
-        this._allowNull = params?.allowNull ?? false;
-        this._value = value;
+    constructor(value: T) {
+        this.value = value;
         this.#ensureIsValidValue();
     }
 
-    abstract setPermittedValues(): void
-
-    value() {
-        return this._value;
-    }
-
     #ensureIsValidValue() {
-        if (g.isNullish(this._value)) {
-            if (!this._allowNull) {
-                const message = '<EnumVo> no permite un valor vacio';
-                throw new InvalidValueException(message);
-            } else {
-                return;
-            }
-        }
-
-        if (typeof this._value !== 'string') {
-            const message = '<EnumVo> ha de ser de tipo String';
-            throw new InvalidValueException(message);
-        }
-
-        if (!this._permittedValues.includes(this._value)) {
+        if (!this._permittedValues.includes(this.value)) {
             const message = `<EnumVo> ha recibido un valor no permitido. Valores permitidos [${this._permittedValues.join(',')}]`;
             throw new InvalidValueException(message);
         }
     }
 
-    static from<T extends EnumVo>(this: new (value: StrOrNullish, params?: EnumVoParams) => T, value: StrOrNullish, params?: EnumVoParams): T {
-        return new this(value, params);
+    static from<V extends StrIntOrNullish, U extends EnumVo<V>>(
+        this: new (value: V) => U,
+        value: V
+    ): U {
+        return new this(value);
     }
 }
