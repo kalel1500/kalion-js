3d295ac13465c4eea06dbeb96f75f5dbec71b2c2
Adrian Canals <a.canals14@gmail.com>
2026-04-21 16:43:02 +0200

    slimSelect: Nuevo parámetro fetchLimit

--- src/app/core/infrastructure/utilities/general/SlimHelper.ts
+++ src/app/core/infrastructure/utilities/general/SlimHelper.ts
@@ -6,14 +6,14 @@ type SearchCallback = (search: string, currentData: SlimData[]) => Promise<Optgr
 
 export class SlimHelper {
 
-    public static debouncedSearch({source, delay = 500, minLength = 3}: {source: string | SearchCallback, delay?: number, minLength?: number}) {
+    public static debouncedSearch({source, delay = 500, minLength = 3, fetchLimit = 20}: {source: string | SearchCallback, delay?: number, minLength?: number, fetchLimit?: number}) {
         const searchLogic = async (search: string, currentData: SlimData[]): Promise<SlimData[]> => {
             if (search.length < minLength) {
                 throw new Error(`Search must be at least ${minLength} characters`);
             }
 
             if (typeof source === 'string') {
-                return await SlimHelper.fetchStandard(source, search, currentData);
+                return await SlimHelper.fetchStandard(source, search, currentData, fetchLimit);
             }
 
             return await source(search, currentData);
@@ -22,7 +22,7 @@ export class SlimHelper {
         return g.debounce(searchLogic, delay);
     }
 
-    private static async fetchStandard(baseUrl: string, search: string, currentData: SlimData[]): Promise<SlimData[]> {
+    private static async fetchStandard(baseUrl: string, search: string, currentData: SlimData[], fetchLimit: number): Promise<SlimData[]> {
         // Extraemos todos los IDs ya seleccionados para filtrar (aplanamos currentData para obtener solo los valores)
         const selectedValues = currentData.flatMap(d =>
             'options' in d ? d.options.map(o => o.value) : [d.value]
@@ -30,7 +30,7 @@ export class SlimHelper {
 
         const url = new URL(baseUrl, window.location.origin);
         url.searchParams.set('search', search);
-        url.searchParams.set('limit', '20');
+        url.searchParams.set('limit', fetchLimit.toString());
         url.searchParams.set('exclude', selectedValues.join(','));
 
 
