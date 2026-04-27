dd4ecb6533b73b07118a9be703b7e7b4896411d3
Adrian Canals <a.canals14@gmail.com>
2026-04-27 18:06:47 +0200

    FModal v2

--- src/app/core/infrastructure/utilities/modals/FModal.ts
+++ src/app/core/infrastructure/utilities/modals/FModal.ts
@@ -4,52 +4,148 @@ import type { ModalOptions, ModalInterface, InstanceOptions } from "flowbite";
 export type CreationOptions = {
     modalOptions?: ModalOptions;
     instanceOptions?: InstanceOptions;
-    onConfirm?: () => boolean;
+    onConfirm?: () => boolean | Promise<boolean>;
+    divMessageId?: string;
+};
+
+export type ShowMessageOptions = {
+    message: string;
+    type: "success" | "error";
+    elementId?: string;
+    autoHide?: boolean;
+    hideAfter?: number;
 };
 
 export default class FModal {
-    static create(id: string, options?: CreationOptions): ModalInterface {
-        const $modalElement: HTMLElement = document.querySelector(`#${id}`) as HTMLElement;
+    public static modalId?: string;
+    public static divMessageId?: string;
+    public static $modalElement?: HTMLElement;
 
-        const defaultModalOptions: ModalOptions = {
-            placement: "center",
-            backdrop: "static",
-            backdropClasses: "bg-gray-900/50 dark:bg-gray-900/80 fixed inset-0 z-40",
-            closable: true,
-            onHide: () => {
-                // console.log("modal is hidden");
+    protected static toggleBtn(action: "disable" | "enable", $btnConfirm: HTMLButtonElement) {
+        const $spinner = $btnConfirm.parentElement?.parentElement?.querySelector('[data-is-spinner="true"]') as HTMLElement;
+        const $spinnerBackdrop = $btnConfirm.parentElement?.parentElement?.querySelector('[data-is-spinner-backdrop="true"]') as HTMLElement;
+        if (action === "disable") {
+            $btnConfirm.disabled = true;
+            $btnConfirm.style.opacity = "0.5";
+            $btnConfirm.style.cursor = "not-allowed";
+            $spinner?.classList.remove("hidden");
+            $spinnerBackdrop?.classList.remove("hidden");
+        }
+        if (action === "enable") {
+            $btnConfirm.disabled = false;
+            $btnConfirm.style.opacity = "1";
+            $btnConfirm.style.cursor = "pointer";
+            $spinner?.classList.add("hidden");
+            $spinnerBackdrop?.classList.add("hidden");
+        }
+    }
+
+    public static create(id: string, options?: CreationOptions): ModalInterface {
+        this.modalId = id;
+        this.divMessageId = options?.divMessageId;
+        this.$modalElement = document.querySelector(`#${id}`) as HTMLElement;
+
+        const modal = new Modal(
+            this.$modalElement,
+            {
+                placement: "center",
+                backdrop: "static",
+                backdropClasses: "bg-dark-backdrop/70 fixed inset-0 z-40", // "bg-gray-900/50 dark:bg-gray-900/80"
+                closable: true,
+                onHide: () => {
+                    // console.log("modal is hidden");
+                    this.clearModal();
+                },
+                onShow: (modal) => {
+                    // console.log("modal is shown");
+                },
+                onToggle: () => {
+                    // console.log("modal has been toggled");
+                },
+                ...options?.modalOptions,
             },
-            onShow: (modal) => {
-                // console.log("modal is shown");
-                $modalElement.addEventListener("click", (e) => {
-                    const $btnHide = (e.target as HTMLElement).closest(`[data-fmodal-cancel="${id}"]`);
-                    const $btnConfirm = (e.target as HTMLElement).closest(`[data-fmodal-confirm="${id}"]`);
-                    if ($btnHide) {
+            {
+                id: id,
+                override: true,
+                ...options?.instanceOptions,
+            },
+        );
+
+        this.$modalElement.addEventListener("click", async (e) => {
+            const target = e.target as HTMLElement;
+            const $btnHide = target.closest(`[data-fmodal-cancel="${id}"]`);
+            const $btnConfirm = target.closest(`[data-fmodal-confirm="${id}"]`) as HTMLButtonElement; // Casteo a HTMLButtonElement
+
+            if ($btnHide) {
+                modal.hide();
+            }
+
+            if ($btnConfirm) {
+                if (!options?.onConfirm) {
+                    modal.hide();
+                    return;
+                }
+
+                this.toggleBtn("disable", $btnConfirm);
+
+                try {
+                    const shouldHide = await options.onConfirm();
+
+                    if (shouldHide) {
                         modal.hide();
                     }
-                    if ($btnConfirm) {
-                        if (options?.onConfirm) {
-                            if (options.onConfirm()) {
-                                modal.hide();
-                            }
-                        } else {
-                            modal.hide();
-                        }
-                    }
-                });
-            },
-            onToggle: () => {
-                // console.log("modal has been toggled");
-            },
-            ...options?.modalOptions,
-        };
+                } catch (error) {
+                    console.error("Error en onConfirm:", error);
+                } finally {
+                    this.toggleBtn("enable", $btnConfirm);
+                }
+            }
+        });
 
-        const defaultInstanceOptions: InstanceOptions = {
-            id: id,
-            override: true,
-            ...options?.instanceOptions,
-        };
+        return modal;
+    }
 
-        return new Modal($modalElement, defaultModalOptions, defaultInstanceOptions);
+    protected static getMessageElement(): HTMLElement | undefined {
+        const $messageElement = this.divMessageId
+            ? (document.querySelector(`#${this.divMessageId}`) as HTMLElement)
+            : (document.querySelector(`.fmodal-message`) as HTMLElement);
+        if (!$messageElement) {
+            console.error(`The element to display messages could not be found.`);
+            return;
+        }
+        return $messageElement;
+    }
+
+    public static showMessage({ message, type, autoHide = true, hideAfter = 3000 }: ShowMessageOptions) {
+        const $messageElement = this.getMessageElement();
+        if (!$messageElement) return;
+
+        $messageElement.textContent = message;
+        $messageElement.classList.remove("hidden", "text-green-600", "text-red-600");
+        $messageElement.classList.add(type === "success" ? "text-green-600" : "text-red-600");
+
+        if (autoHide) {
+            setTimeout(() => {
+                $messageElement.classList.add("hidden");
+            }, hideAfter);
+        }
+    }
+
+    public static hideMessage() {
+        const $messageElement = this.getMessageElement();
+        if (!$messageElement) return;
+
+        $messageElement.classList.add("hidden");
+    }
+
+    public static clearModal() {
+        this.hideMessage();
+
+        const inputs = this.$modalElement?.querySelectorAll<HTMLInputElement | HTMLTextAreaElement>("input, textarea");
+        inputs?.forEach((input) => {
+            if (input.type !== "hidden") {
+                input.value = "";
+            }
+        });
     }
 }
