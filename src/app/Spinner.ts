9b8d05c77618b21cdd96e1e3e1aa99da032604ca
Adrian Canals <a.canals14@gmail.com>
2026-06-22 10:42:04 +0200

    spinner: primera version de la clase Spinner

---
+++ src/app/Spinner.ts
@@ -0,0 +1,29 @@
+﻿
+export class Spinner {
+    protected readonly $spinner: HTMLElement;
+    protected readonly $spinnerBackdrop: HTMLElement;
+
+    constructor(parent: string | HTMLElement) {
+        const parentElement = this.resolveElement(parent, "parent") as HTMLElement;
+
+        this.$spinner = parentElement?.querySelector('[data-is-spinner="true"]') as HTMLElement;
+        this.$spinnerBackdrop = parentElement?.querySelector('[data-is-spinner-backdrop="true"]') as HTMLElement;
+    }
+
+    private resolveElement(target: string | HTMLElement | HTMLInputElement | undefined, name: string): Element {
+        if (target === undefined) throw new Error(`[Spinner] The "${name}" parameter is required.`);
+        const el = typeof target === "string" ? document.querySelector(target) : target;
+        if (!el) throw new Error(`[Spinner] No element found for "${name}" with selector: "${target}".`);
+        return el;
+    }
+
+    public show() {
+        this.$spinner?.classList.remove("hidden");
+        this.$spinnerBackdrop?.classList.remove("hidden");
+    }
+
+    public hide() {
+        this.$spinner?.classList.add("hidden");
+        this.$spinnerBackdrop?.classList.add("hidden");
+    }
+}
