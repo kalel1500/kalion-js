e5acdc511a48b5571b3a8afacf142da7e8c0f682
Adrian Canals <a.canals14@gmail.com>
2026-09-08 18:44:31 +0200

    Mejorar clase TestConstants

--- src/app/tests/TestConstants.ts
+++ src/app/tests/TestConstants.ts
@@ -2,25 +2,28 @@ import { __const } from '@/app/_internal/helpers';
 
 export class TestConstants {
     private property_value: string = __const('VITE_APP_NAME');
-    private constructor_value: string;
 
-    constructor() {
-        this.constructor_value = __const('VITE_APP_NAME');
-    }
+    private static static_property_value: string = __const('VITE_APP_NAME');
 
-    printPropertyValue()
+    private static function_property_value = () => __const('VITE_APP_NAME');
+
+    public printPropertyValue()
     {
         console.log(this.property_value);
     }
 
-    printConstructorValue()
-    {
-        console.log(this.constructor_value);
-    }
-
-    printMethodValue()
+    public printMethodValue()
     {
         console.log(__const('VITE_APP_NAME'));
     }
 
+    public static printStaticPropertyValue()
+    {
+        console.log(TestConstants.static_property_value);
+    }
+
+    public static printFunctionPropertyValue()
+    {
+        console.log(TestConstants.function_property_value());
+    }
 }
